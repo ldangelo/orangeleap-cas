d1863a5dcf808b5b4ea0d2437984e60540aacd57
tsporcic <tsporcic@0f581344-5646-3a48-8ded-616bfea576c6>
2009-05-28 20:37:29 +0000

    Added custom LDAP authenticator, updated to CAS 3.3.2 and included additional jars for LDAP.
    git-svn-id: svn://svn.orangeleap.com/tang/trunk@6111 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/orangeleap/security/cas/TangerineAuthenticationHandler.java
@@ -0,0 +1,48 @@
+package com.orangeleap.security.cas;
+
+import org.jasig.cas.adaptors.ldap.AbstractLdapUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
+import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.handler.BadCredentialsAuthenticationException;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+import org.springframework.security.userdetails.ldap.LdapUserDetailsMapper;
+import org.springframework.security.userdetails.ldap.LdapUserDetailsImpl;
+import org.springframework.ldap.filter.EqualsFilter;
+import org.springframework.ldap.filter.Filter;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.core.DistinguishedName;
+
+import java.text.MessageFormat;
+
+/**
+ *  Custom LDAP Authentication Handler that knows what to do with user names
+ *  in the format user@site. Will split them out correctly for the LDAP search
+ */
+public class TangerineAuthenticationHandler extends AbstractLdapUsernamePasswordAuthenticationHandler {
+
+    private static final Log logger = LogFactory.getLog(TangerineAuthenticationHandler.class);
+
+    @Override
+    protected boolean authenticateUsernamePasswordInternal(UsernamePasswordCredentials credentials) throws AuthenticationException {
+
+        String userName = credentials.getUsername();
+        String password = credentials.getPassword();
+        logger.info("Authenticating User: " + userName);
+
+        // split the username in the userid and site components
+        String[] args = userName.split("@");
+
+        // if we don't have both a username and a site, assume an error
+        if(args.length < 2) {
+            logger.warn("Authentication attempted without a site parameter: " + userName);
+            throw new BadCredentialsAuthenticationException("Username must be in the format of user@site");
+        }
+
+        Filter filter = new EqualsFilter("uid", args[0]);
+
+        return getLdapTemplate().authenticate( String.format("o=%s",args[1]), filter.toString(), password );
+
+    }
+
+}
--- web/authorizationFailure.jsp
+++ web/authorizationFailure.jsp
@@ -4,7 +4,7 @@
 <jsp:directive.include file="/WEB-INF/view/jsp/default/ui/includes/top.jsp" />
 <div class="errors" id="status">
 	<h2>Authorization Failure</h2>
-	<p>You are not authorized to use this application for the following reason:
+	<p>You are not authorized to use this application for the following reason: 
 	<%final Exception e = (Exception) request.getSession().getAttribute(AbstractProcessingFilter.SPRING_SECURITY_LAST_EXCEPTION_KEY);%>
 	<%=e.getMessage()%>.
 	</p>
