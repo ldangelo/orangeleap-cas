cd6c42519dc8a2a64f69d9d92d9286a5dd67f5b5
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-09-28 22:01:29 +0000

    prevent resetting of cookiePath
    git-svn-id: svn://svn.orangeleap.com/tang/trunk@8096 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/orangeleap/security/cookie/RootPathCookieRetrievingCookieGenerator.java
@@ -0,0 +1,10 @@
+package com.orangeleap.security.cookie;
+
+public class RootPathCookieRetrievingCookieGenerator extends org.jasig.cas.web.support.CookieRetrievingCookieGenerator {
+	
+	@Override
+	public void setCookiePath(String s) {
+		// Dont allow InitialFlowSetupAction to set cookie path to CAS context path
+		super.setCookiePath("\\");
+	}
+}
